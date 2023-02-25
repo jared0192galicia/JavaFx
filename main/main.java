8b47619db58343e00cc9b1ca5018c658f6955c34
MATEBOOK-13\jared <jared0192galicia@gmail.com>
2023-02-25 02:41:10 -0600

    Use borderPane as Frame, and check diferents pointers

--- main/main.java
+++ main/main.java
@@ -5,13 +5,13 @@
  */
 package main;
 
+import javafx.geometry.Insets;
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.LineTo;
-import javafx.scene.shape.MoveTo;
-import javafx.scene.shape.Path;
 import javafx.stage.Stage;
 
 /**
@@ -25,37 +25,63 @@ public class main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws InterruptedException {
         
-        primaryStage.setTitle("Prueba");
+        // Create buttons for frame form
+        Button button1 = new Button("boton 1");
+        Button button2 = new Button("boton 2");
+        Button button3 = new Button("boton 3");
+        Button button4 = new Button("boton 4");
+        Button button5 = new Button("boton 5");
         
-        // Create node label
-        Path path = new Path();
+        BorderPane root = new BorderPane();
         
-        MoveTo moveTo = new MoveTo(108, 71);
+        // Add buttons to layout
+        root.setCenter(button1);
+        button1.setCursor(Cursor.CROSSHAIR);
+        button1.setMaxHeight(Double.MAX_VALUE);
+        button1.setMaxWidth(Double.MAX_VALUE);
+        BorderPane.setMargin(button1, new Insets(5, 5, 5, 5));
         
-        LineTo line1 = new LineTo(321, 161);
-        LineTo line2 = new LineTo(126, 232);
-        LineTo line3 = new LineTo(232, 52);
-        LineTo line4 = new LineTo(269, 250);
-        LineTo line5 = new LineTo(108, 71);
+        root.setBottom(button2);
+        button2.setCursor(Cursor.DISAPPEAR);
+        button2.setMaxHeight(Double.MAX_VALUE);
+        button2.setMaxWidth(Double.MAX_VALUE);
+        BorderPane.setMargin(button1, new Insets(5, 5, 5, 5));
         
-        path.getElements().add(moveTo);
-        path.getElements().addAll(line1, line2, line3, line4, line5);
+        root.setLeft(button3);
+        button3.setCursor(Cursor.OPEN_HAND);
+        button3.setMaxHeight(Double.MAX_VALUE);
+        button3.setMaxWidth(Double.MAX_VALUE);
+        button3.setOnMouseClicked((t) -> {
+            newWindow window = new newWindow();
+            System.out.println("Create instance");
+        });
+        BorderPane.setMargin(button1, new Insets(5, 5, 5, 5));
         
-        // Root node
-        Group root = new Group(path);
+        root.setRight(button4);
+        button4.setCursor(Cursor.WAIT);
+        button4.setMaxHeight(Double.MAX_VALUE);
+        button4.setMaxWidth(Double.MAX_VALUE);
+        BorderPane.setMargin(button1, new Insets(5, 5, 5, 5));
         
+        root.setTop(button5);
+        button5.setCursor(Cursor.CLOSED_HAND);
+        button5.setMaxHeight(Double.MAX_VALUE);
+        button5.setMaxWidth(Double.MAX_VALUE);
+        BorderPane.setMargin(button1, new Insets(5, 5, 5, 5));
         
-        // Add label at node root
-        //root.getChildren().add(label);
-        // Main node: Scene
         Scene scene = new Scene(root, 600, 300);
         
         scene.setFill(Color.AQUA);
         
         // Add Scene at stage and show UI: setVisible
         primaryStage.setScene(scene);
+        primaryStage.setTitle("Prueba");
         primaryStage.show();
+        
+        root.setOpacity(0.9);
+        // Main node: Scene
+        
     }
 }
